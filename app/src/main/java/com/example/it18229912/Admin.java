e390e31eebad887c93183bf07ebdd3f996f82d84
W Naween Tharuka <naweentharuka319@gmail.com>
2020-03-19 12:07:48 +0530

    vehicle Management

---
+++ app/src/main/java/com/example/it18229912/Admin.java
@@ -0,0 +1,28 @@
+package com.example.it18229912;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class Admin extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin);
+
+        Button go = (Button) findViewById(R.id.button3);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(Admin.this,Edit.class);
+
+                startActivity(intent);
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/it18229912/MainActivity.java
@@ -0,0 +1,27 @@
+package com.example.it18229912;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button go = (Button) findViewById(R.id.button2);
+
+        go.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,Admin.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
